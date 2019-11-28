9f4932f195904580a772b72482fdaf7410109cf1
berukokoko <beru2491@gmail.com>
2019-11-28 23:39:39 +0900

    修正

---
+++ resources/views/hello/edit.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.helloapp')
+
+@section('title','Edit')
+
+@section('menuber')
+  @parent
+  更新ページ
+@endsection
+
+@section('content')
+<table>
+<form action ="/hello/add" method="post">
+  {{csrf_field()}}
+  <input type="hidden" name="id" value="{{$from->id}}">
+<tr><th>name:</th><td><input type="text" name="name" value="{{$from->name}}"></td></tr>
+<tr><th>mail:</th><td><input type="text" name="mail" value="{{$from->mail}}"></td></tr>
+<tr><th>age:</th><td><input type="text" name="age" value="{{$from->age}}"></td></tr>
+<tr><th></th><td><input type="submit" value="send"></td></tr>
+</form>
+</table>
+@endsection
+
+@section('footer')
+copyright 2017 tuyano.
+@endsection
