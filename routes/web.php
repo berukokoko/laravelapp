e4dd10d2c8b601ed1d24f38430605510f54bbb03
berukokoko <beru2491@gmail.com>
2019-12-02 18:50:02 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
  use App\Http\Middleware\HelloMiddleware;
 
 
-// Route::get('hello/del','HelloController@del');
-// Route::post('hello/del','HelloController@remove');
+Route::get('hello/del','HelloController@del');
+Route::post('hello/del','HelloController@remove');
 
 Route::get('hello','HelloController@index');
 Route::post('hello','HelloController@index');
@@ -15,5 +15,5 @@ Route::post('hello','HelloController@index');
 //Route::get('hello/edit','HelloController@edit');
 //Route::post('hello/edit','HelloController@edit');
 
-Route::get('hello/edit','HelloController@edit');
-Route::post('hello/edit','HelloController@update');
+// Route::get('hello/edit','HelloController@edit');
+// Route::post('hello/edit','HelloController@update');
