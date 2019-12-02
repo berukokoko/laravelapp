f6e6b1e2433fcc2e8b45314f78bf09107cd4f9b6
berukokoko <beru2491@gmail.com>
2019-12-02 18:37:17 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,15 @@
 // Route::get('hello/del','HelloController@del');
 // Route::post('hello/del','HelloController@remove');
 
-//Route::get('hello','HelloController@index');
+Route::get('hello','HelloController@index');
+Route::post('hello','HelloController@index');
+//Route::get('hello/show','HelloController@show');
 
-Route::get('hello/show','HelloController@show');
+// Route::get('hello/add','HelloController@add');
+// Route::post('hello/add','HelloController@create');
+
+//Route::get('hello/edit','HelloController@edit');
+//Route::post('hello/edit','HelloController@edit');
+
+Route::get('hello/edit','HelloController@edit');
+Route::post('hello/edit','HelloController@update');
